cdc3112ccfeeb052d41aa8dbd0beeb4904962f09
Charles Glass <charlie-glass@live.com>
2021-08-29 11:57:00 -0700

    fixed model index to correctly relate the tables

--- models/index.js
+++ models/index.js
@@ -4,16 +4,10 @@ const Ingredient = require ('./Ingredient');
 const Type = require ('./Type');
 
 
-User.hasMany(Drink,{
-    foreignKey: 'drink_id',
-});
+User.hasMany(Drink);
+Drink.belongsTo(User);
 
-Drink.belongsToMany(User,{
-    through: 'user_id',
-});
-
-Ingredient.hasOne(Type, {
-    foreignKey: 'ingredient_type'
-});
+Ingredient.hasOne(Type);
+Type.belongsTo(Ingredient)
 
 module.exports = { User, Drink, Ingredient, Type }
