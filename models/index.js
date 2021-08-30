9380fc80859b79d037f7a361f9d194937d015f2f
Charles Glass <charlie-glass@live.com>
2021-08-30 16:59:37 -0700

    added amount to each ingredient in drink table

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+const sequelize = require('../config/connection.js');
+var DataTypes = require('sequelize/lib/data-types');
 const User = require('./User');
 const Drink = require('./Drink');
 const Ingredient = require ('./Ingredient');
@@ -11,12 +13,14 @@ Type.hasMany(Ingredient);
 Ingredient.belongsTo(Type)
 
 
-// const drinkIngredients = sequelize.define('drinkIngredients', {
-//     ammount: DataTypes.INTEGER
-//   });
+const drinkIngredients = sequelize.define('drinkIngredients', {
+    amount: {type:DataTypes.INTEGER, allowNull: false}
+  })
 
 Ingredient.belongsToMany(Drink, { through: 'drinkIngredients'});
 Drink.belongsToMany(Ingredient,{ through: 'drinkIngredients'})
 
 
 module.exports = { User, Drink, Ingredient, Type }
+
+
