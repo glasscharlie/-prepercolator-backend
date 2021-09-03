783876d2d3c898cfe68ce160412726b01cf0a610
Atlas <jack@bentsen.us>
2021-09-02 18:06:39 -0700

    fixing-drink-seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,40 +1,111 @@
-const sequelize = require('../config/connection');
-const { User, Ingredient, Type, Drink, Order } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Ingredient, Type, Drink, Order } = require("../models");
 
-const userSeedData = require('./userSeedData.json');
-const ingredientSeedData = require('./ingredientSeedData.json');
-const typeSeedData = require('./typeSeedData.json');
-const drinkSeedData = require('./drinkSeedData.json');
-const orderSeedData = require('./orderSeedData.json');
+const userSeedData = require("./userSeedData.json");
+const ingredientSeedData = require("./ingredientSeedData.json");
+const typeSeedData = require("./typeSeedData.json");
+const drinkSeedData = require("./drinkSeedData.json");
+const orderSeedData = require("./orderSeedData.json");
 
 const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-    await sequelize.sync({ force: true });
+  await User.bulkCreate(userSeedData);
 
-    await User.bulkCreate(userSeedData)
+  await Type.bulkCreate(typeSeedData);
 
-    await Type.bulkCreate(typeSeedData);
+  await Ingredient.bulkCreate(ingredientSeedData);
 
-    await Ingredient.bulkCreate(ingredientSeedData);
+  await Drink.create(drinkSeedData[0]).then(async (drink) => {
+    ingredients = [1, 2, 12];
+    ingredient_amount = [5, 3, 1];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[1]).then(async (drink) => {
+    ingredients = [2];
+    ingredient_amount = [16];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[2]).then(async (drink) => {
+    ingredients = [2, 6];
+    ingredient_amount = [2, 4];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[3]).then(async (drink) => {
+    ingredients = [1, 6];
+    ingredient_amount = [2, 8];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[4]).then(async (drink) => {
+    ingredients = [1, 6, 12, 13];
+    ingredient_amount = [2, 8, 2, 2];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[5]).then(async (drink) => {
+    ingredients = [1, 8];
+    ingredient_amount = [2, 8];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[6]).then(async (drink) => {
+    ingredients = [1, 9];
+    ingredient_amount = [2, 8];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[7]).then(async (drink) => {
+    ingredients = [1, 9, 12];
+    ingredient_amount = [3, 8, 2];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
+  await Drink.create(drinkSeedData[8]).then(async (drink) => {
+    ingredients = [1, 21];
+    ingredient_amount = [2, 1];
+    for (let i = 0; i < ingredients.length; i++) {
+      await drink.addIngredient(ingredients[i], {
+        through: { amount: ingredient_amount[i] },
+      });
+    }
+  });
 
-    await Drink.create(drinkSeedData[0]).then(async drink => {
-        ingredients = [1,2,12]
-        ingredient_amount = [5,3,1]
-        for (let i = 0; i < ingredients.length; i++) {
-            await drink.addIngredient(ingredients[i], { through: { amount:ingredient_amount[i]} } )            
-        }
-    });
+  await Order.create(orderSeedData).then(async (order) => {
+    drinks = [1, 2, 2];
+    for (let i = 0; i < drinks.length; i++) {
+      await order.addDrinks(drinks[i]);
+    }
+  });
 
-    await Drink.create(drinkSeedData[1])
-
-    await Order.create(orderSeedData).then(async order => {
-        drinks = [1,2,2]
-        for (let i = 0; i < drinks.length; i++) {
-            await order.addDrinks(drinks[i])            
-        }
-    });
-
-    process.exit(0);
+  process.exit(0);
 };
 
 seedDatabase();
